1cad6220b8b5469692fedfdd91add2cd2f042048
Antonio de la Rosa <webmaster@web-t-sys.com>
2016-03-03 21:41:18 +0100

    Little silly fix in DateField

--- src/CoreFields/DateField.php
+++ src/CoreFields/DateField.php
@@ -54,10 +54,10 @@ class DateField extends PhangoField {
 			
 			if($value[0]>0 && $value[1]>0 && $value[2]>0)	
 			{
-			
+                
                 $new_timestamp=date(PhaTime\DateTime::$sql_format_time, mktime($value[3], $value[4], $value[5], $value[1], $value[0], $value[2]));
 
-				$final_value=PhaTime\DateTime::local_to_gmt( $new_timestamp );
+				$value=PhaTime\DateTime::local_to_gmt( $new_timestamp );
                 
 			}
 
